90b992443f9ac734f9fbe29812bde14f85b45467
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-10-26 11:00:33 +0300

    Task2.

--- task.js
+++ task.js
@@ -1,24 +1,24 @@
-function parseCount(str){
-   
+function parseCount(str) {
+
   let result = Number.parseInt(str);
-  
-  if (isNaN(result)){
+
+  if (isNaN(result)) {
     console.log('Невалидное значение');
     //throw 'Невалидное значение'; //???почему с throw  не выводится 'Невалидное значение'
   }
   return result;
-  
+
 }
 let obj = '21';
 console.log(parseCount(obj));
 
 function validateCount(str) {
-  let res = parseCount(str);
-  
-  if (!isNaN(res)){
+  try {
+    let res = parseCount(str);
     return res;
-  } else {
-    return 'Невалидное значение';
- }
-                         
+  } catch (error) {
+    return error;
   }
+
+}
+console.log(validateCount(obj));
