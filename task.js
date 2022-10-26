5ee0db4a69baef2a1d112ebb6b76dc860ed08d9b
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-10-26 15:41:05 +0300

    All

--- task.js
+++ task.js
@@ -1,15 +1,13 @@
 function parseCount(str) {
-
   let result = Number.parseInt(str);
 
   if (isNaN(result)) {
-    console.log('Невалидное значение');
-    //throw 'Невалидное значение'; //???почему с throw  не выводится 'Невалидное значение'
+    console.log("Невалидное значение");
+    //throw 'Невалидное значение'; //  ???почему с throw  не выводится 'Невалидное значение'???
   }
   return result;
-
 }
-let obj = '21';
+let obj = "21";
 console.log(parseCount(obj));
 
 function validateCount(str) {
@@ -19,6 +17,57 @@ function validateCount(str) {
   } catch (error) {
     return error;
   }
-
 }
 console.log(validateCount(obj));
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  /*if ((this.a + this.b) < this.c || (this.a + this.c) < this.b || (this.b + this.c) < this.a){
+    throw "Треугольник с такими сторонами не существует";
+  }//    ??? Что у меня не верно записано в if(...)???     */
+  getPerimeter() {
+    this.perim = this.a + this.b + this.c;
+    return this.perim;
+  }
+  getArea() {
+    this.p = this.perim / 2;
+    this.S = Math.sqrt(
+      this.p * (this.p - this.a) * (this.p - this.b) * (this.p - this.c)
+    );
+    console.log(Number(this.S.toFixed(3)));
+  }
+}
+
+let g = new Triangle(2, 2, 2);
+g.getPerimeter();
+console.log(g.getArea());
+
+function getTriangle(a, b, c) {
+  try {
+    let dw = new Triangle(a, b, c);
+
+    return dw;
+  } catch (error) {
+    let ob = {};
+    ob.getArea = function (a, b, c) {
+      let perimetrHalf = (a + b + c) / 2;
+      let S = Math.sqrt(
+        perimetrHalf *
+          (perimetrHalf - a) *
+          (perimetrHalf - b) *
+          (perimetrHalf - c)
+      );
+    };
+    ob.getPerimetr = function (a, b, c) {
+      let perim = a + b + c;
+      return perim;
+    };
+
+    return ob;
+  }
+}
+console.log(getTriangle(2, 2, 2));
